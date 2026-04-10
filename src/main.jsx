639fe1227f2b06451ecaee5f8b0a6db23cd29652
Dmitry Sharsniou <shersnevbm@gmail.com>
2026-04-10 09:57:22 +0300

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/YouTube-SPA/">
     <Provider store={store}>
       <App />
     </Provider>
